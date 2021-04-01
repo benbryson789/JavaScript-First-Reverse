d2acc7a6ebdce0ec116d079e73390bbb9bca568a
Debra Shelby <deborashelby@gmail.com>
2021-03-31 21:45:35 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function FirstReverse(str) { 
+   
+    return str.split("").reverse().join("")
+  
+  }
+  
+  
+  console.log(FirstReverse("coderbyte"))//etybredoc
+     
+  // keep this function call here 
+  //console.log(FirstReverse(readline()));
+  
